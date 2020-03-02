77e41d4c4d6de1390bd4b88ef2edb67b35d918e0
drwolf1999 <drwolf1888@gmail.com>
2020-03-02 15:51:54 +0900

    remove files

--- projectname1/account/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Account(models.Model):
-    userId = models.CharField(primary_key=True, unique=True, max_length=15)
-    password = models.CharField(max_length=15)
-    nick = models.CharField(max_length=10)
